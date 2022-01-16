ddc1b59c49bc1b73bc77473ff267ffd87f2798dc
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2022-01-17 00:26:56 +0100

    Add files via upload

---
+++ Læreseg/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("hva heter du?");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("Hei " + firstName + " Hva er etternavnet ditt ?");
+            string lastName = Console.ReadLine();
+           
+            string name = firstName + " " + lastName;
+            Console.WriteLine("Hei " + name);
+
+
+            /*
+            Console.WriteLine("Hva heter du?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Hei " + firstName + " Hva er etternavnet ditt?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hei " + firstName + " " + lastName);
+            */
+
+
+
+
+            /* int a = 0;
+            int b = 0;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+            Console.ReadLine(); */
+
+
+           
+
+
+        }
+    }
+}
